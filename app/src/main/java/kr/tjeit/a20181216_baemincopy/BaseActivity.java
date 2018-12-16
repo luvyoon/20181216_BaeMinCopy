538a407ae065909ef0fdfbe3b62bc12cbab537b6
Hottory <gkffndnl123@naver.com>
2018-12-16 13:57:35 +0900

    Initial commit

---
+++ app/src/main/java/kr/tjeit/a20181216_baemincopy/BaseActivity.java
@@ -0,0 +1,13 @@
+package kr.tjeit.a20181216_baemincopy;
+
+import android.support.v7.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public  abstract void  setupEvent ();
+    public  abstract void  setValues ();
+    public abstract void bindViews();
+
+
+
+}
---
+++ app/src/main/java/kr/tjeit/a20181216_baemincopy/RestaurantListActivity.java
@@ -0,0 +1,32 @@
+package kr.tjeit.a20181216_baemincopy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantListActivity extends BaseActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurant_list);
+
+        bindViews();
+        setupEvent();
+        setValues();
+    }
+
+    @Override
+    public void setupEvent() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+    }
+}
