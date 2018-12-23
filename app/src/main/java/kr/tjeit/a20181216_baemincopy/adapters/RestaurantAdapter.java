842f1626c61b28e6047058f935df2c11e5b1fbe6
Hottory <gkffndnl123@naver.com>
2018-12-23 11:51:20 +0900

    getView 메쏘드 내부 작성 - 화면에 올바른 데이터 출력

--- app/src/main/java/kr/tjeit/a20181216_baemincopy/adapters/RestaurantAdapter.java
+++ app/src/main/java/kr/tjeit/a20181216_baemincopy/adapters/RestaurantAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -14,32 +15,40 @@ import kr.tjeit.a20181216_baemincopy.datas.Restaurant;
 public class RestaurantAdapter extends ArrayAdapter<Restaurant> {
 
 
-
     Context mContext;
     List<Restaurant> mList;
     LayoutInflater inf;
 
-    public RestaurantAdapter(Context mContext, List<Restaurant> mList){
+    public RestaurantAdapter(Context mContext, List<Restaurant> mList) {
 
         super(mContext, R.layout.restaurant_list_item, mList);
 
 
         this.mContext = mContext;
-        this.mList =mList;
+        this.mList = mList;
         this.inf = LayoutInflater.from(mContext);
 
     }
 
 
-
     @Override
-    public View getView(int position,View convertView,  ViewGroup parent) {
+    public View getView(int position, View convertView, ViewGroup parent) {
 
         View row = convertView;
         if (row == null) {
 
             row = inf.inflate(R.layout.restaurant_list_item, null);
         }
+
+        TextView nameTxt = row.findViewById(R.id.nameTxt);
+        TextView addressTxt = row.findViewById(R.id.addressTxt);
+        TextView openTimeTxt = row.findViewById(R.id.openTimeTxt);
+
+        Restaurant data = mList.get(position);
+
+        nameTxt.setText(data.getName());
+        addressTxt.setText(data.getAddress());
+        openTimeTxt.setText(data.getOpenTime());
 
         return row;
 
