0d0a170d4bca033e68399937fbdc328399b2b91b
Hottory <gkffndnl123@naver.com>
2018-12-23 11:17:04 +0900

    onClickListener를 변수에 담아 이벤트 일괄 처리

--- app/src/main/java/kr/tjeit/a20181216_baemincopy/MainActivity.java
+++ app/src/main/java/kr/tjeit/a20181216_baemincopy/MainActivity.java
@@ -1,7 +1,7 @@
 package kr.tjeit.a20181216_baemincopy;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.icu.util.ChineseCalendar;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.LinearLayout;
@@ -12,18 +12,21 @@ public class MainActivity extends BaseActivity {
     LinearLayout koreanFoodBtn;
     LinearLayout bunsikBtn;
     LinearLayout japanesFoodBtn;
+    private LinearLayout chickenBtn;
+    private LinearLayout pizzaBtn;
+    private LinearLayout chineseBtn;
+    private LinearLayout pigBtn;
+    private LinearLayout yasikBtn;
+    private LinearLayout stewBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         bindViews();
         setupEvent();
         setValues();
-
-
-
-
 
 
     }
@@ -32,35 +35,68 @@ public class MainActivity extends BaseActivity {
     @Override
     public void setupEvent() {
 
-        //        한식 버튼이 눌렸을때 할일.
-        koreanFoodBtn.setOnClickListener(new View.OnClickListener() {
+//        //        한식 버튼이 눌렸을때 할일.
+//        koreanFoodBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+////                Toast.makeText(mContext, "한식을 눌렀습니다.", Toast.LENGTH_SHORT).show();
+//
+////                한식이 눌렸다는 사실을 레스토랑 리스트 액티비티에 공지(알리고 싶음)
+//
+//                Intent intent = new Intent(mContext, RestaurantListActivity.class
+//                );
+//                intent.putExtra("음식종류", "한식");
+//                startActivity(intent);
+//
+//            }
+//        });
+
+
+
+
+        View.OnClickListener foodKindClickListener = new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                Toast.makeText(MainActivity.this, "한식을 눌렀습니다.", Toast.LENGTH_SHORT).show();
 
-                Intent intent = new Intent(MainActivity.this,RestaurantListActivity.class
-                );
-                startActivity(intent);
+
+
+// 첫번쨰 방법 예시
+//                if (v.getId() == R.id.chickenBtn){
+//                    Toast.makeText(mContext, "치킨 눌림", Toast.LENGTH_SHORT).show();
+//                } else if (v.getId() == R.id.pizzaBtn){
+//
+//                    Toast.makeText(mContext, "피자 눌림", Toast.LENGTH_SHORT).show();
+//                }
+
+//                두번쨰 방법,xml에 태그 단거 이용하기
+
+                Toast.makeText(mContext, v.getTag().toString(), Toast.LENGTH_SHORT).show();
+
+
+
 
             }
-        });
+        };
 
-        bunsikBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "분식을 눌렀습니다.", Toast.LENGTH_SHORT).show();
-            }
-        });
 
-        japanesFoodBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this, "돈까스.회.일식을 눌렀습니다.", Toast.LENGTH_SHORT).show();
-            }
-        });
+        koreanFoodBtn.setOnClickListener(foodKindClickListener);
+        bunsikBtn.setOnClickListener(foodKindClickListener);
+        japanesFoodBtn.setOnClickListener(foodKindClickListener);
+        chickenBtn.setOnClickListener(foodKindClickListener);
+        pigBtn.setOnClickListener(foodKindClickListener);
+        yasikBtn.setOnClickListener(foodKindClickListener);
+        stewBtn.setOnClickListener(foodKindClickListener);
+        pizzaBtn.setOnClickListener(foodKindClickListener);
+        chineseBtn.setOnClickListener(foodKindClickListener);
+
+
     }
 
+
+
+
     @Override
+
     public void setValues() {
 
     }
@@ -70,8 +106,14 @@ public class MainActivity extends BaseActivity {
 
 //        사실 읽을 필요가 전혀 없는 코드들.
 //        따로 메쏘드를 만들어서 밑으로 치워버리자.
-        koreanFoodBtn = findViewById(R.id.koreanFoodBtn);
-        bunsikBtn = findViewById(R.id.bunsikBtn);
-       japanesFoodBtn = findViewById(R.id.japanesFoodBtn);
+        this.stewBtn = (LinearLayout) findViewById(R.id.stewBtn);
+        this.yasikBtn = (LinearLayout) findViewById(R.id.yasikBtn);
+        this.pigBtn = (LinearLayout) findViewById(R.id.pigBtn);
+        this.chineseBtn = (LinearLayout) findViewById(R.id.chineseBtn);
+        this.pizzaBtn = (LinearLayout) findViewById(R.id.pizzaBtn);
+        this.chickenBtn = (LinearLayout) findViewById(R.id.chickenBtn);
+        this.japanesFoodBtn = (LinearLayout) findViewById(R.id.japanesFoodBtn);
+        this.bunsikBtn = (LinearLayout) findViewById(R.id.bunsikBtn);
+        this.koreanFoodBtn = (LinearLayout) findViewById(R.id.koreanFoodBtn);
     }
 }
